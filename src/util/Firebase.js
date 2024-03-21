980619130d5c7c2468a482e18ff6a2d67202867c
saulotarsobc <saulotarsobc@gmail.com>
2024-03-21 09:57:18 -0300

    :fire: npm run build

--- src/util/Firebase.js
+++ src/util/Firebase.js
@@ -1,68 +1,52 @@
-import * as firebase from 'firebase'
-import * as firestore from 'firebase/firestore'
+import * as firebase from "firebase";
+import * as firestore from "firebase/firestore";
 
 export class Firebase {
+  constructor() {
+    this.init();
+  }
 
-    constructor() {
+  init() {
+    if (!window._initializedFirebase) {
+      firebase.initializeApp({
+        apiKey: "AIzaSyCxQ0lj_mDPaoO0BwiD6fmamqmczzGWVlY",
+        authDomain: "whatsapp-clone-a7181.firebaseapp.com",
+        projectId: "whatsapp-clone-a7181",
+        storageBucket: "gs://whatsapp-clone-a7181.appspot.com/",
+      });
 
-        this.init();
+      firebase.firestore().settings({
+        timestampsInSnapshots: true,
+      });
 
+      window._initializedFirebase = true;
     }
+  }
 
-    init(){
+  initAuth() {
+    return new Promise((resolve, reject) => {
+      let provider = new firebase.auth.GoogleAuthProvider();
 
-        if (!window._initializedFirebase) {
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(function (result) {
+          let token = result.credential.accessToken;
+          let user = result.user;
 
-            firebase.initializeApp({
-                apiKey: 'AIzaSyCxQ0lj_mDPaoO0BwiD6fmamqmczzGWVlY',
-                authDomain: 'whatsapp-clone-a7181.firebaseapp.com',
-                projectId: 'whatsapp-clone-a7181',
-                storageBucket: 'gs://whatsapp-clone-a7181.appspot.com/'
-            });
+          resolve(user, token);
+        })
+        .catch(function (error) {
+          reject(error);
+        });
+    });
+  }
 
-            firebase.firestore().settings({
-                timestampsInSnapshots: true
-            });
+  static db() {
+    return firebase.firestore();
+  }
 
-            window._initializedFirebase = true;
-
-        }
-
-    }
-
-    initAuth(){
-
-        return new Promise((resolve, reject)=>{
-
-            let provider = new firebase.auth.GoogleAuthProvider();
-
-            firebase.auth().signInWithPopup(provider).then(function (result) {
-
-                let token = result.credential.accessToken;
-                let user = result.user;
-
-                resolve(user, token);
-
-            }).catch(function (error) {
-
-                reject(error);
-
-            });
-
-        });        
-
-    }
-
-    static db(){
-
-        return firebase.firestore();
-
-    }
-
-    static hd() {
-
-        return firebase.storage();
-
-    }
-
+  static hd() {
+    return firebase.storage();
+  }
 }
